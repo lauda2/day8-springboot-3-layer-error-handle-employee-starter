b1e32c0d2ecf112164fcafb2c73894d7dabb750b
Danny Lau <lauda2@oocl.com>
2025-09-11 10:33:48 +0800

    fix: add back responseStatusExceptionHandler

--- src/main/java/com/example/demo/controller/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/controller/advice/GlobalExceptionHandler.java
@@ -5,20 +5,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseException responseStatusExceptionHandler(Exception e) {
+        return new ResponseException(e.getMessage());
+    }
 
     @ExceptionHandler(InvalidEmployeeException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public InvalidEmployeeException invalidEmployeeExceptionHandler(Exception e) {
         return new InvalidEmployeeException(e.getMessage());
-    }
-
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String exceptionHandler(Exception e) {
-        return e.getMessage();
     }
 
 }
