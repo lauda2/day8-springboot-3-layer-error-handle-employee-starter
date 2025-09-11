7cc4090a0f6f90db3d07348237dc9acc93d6ff7d
Danny Lau <lauda2@oocl.com>
2025-09-11 15:25:51 +0800

    refactor: employee mapper

---
+++ src/main/java/com/example/demo/dto/EmployeeResponse.java
@@ -0,0 +1,52 @@
+package com.example.demo.dto;
+
+public class EmployeeResponse {
+
+    public EmployeeResponse() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String gender;
+    private boolean active = true;
+}
